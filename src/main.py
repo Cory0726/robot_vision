3c123ce8450ff71f1d025a9b0ee6fe43bae01d90
User <lkfu520@gmail.com>
2025-11-18 16:24:04 +0800

    update

--- src/main.py
+++ src/main.py
@@ -12,12 +12,15 @@ if __name__ == '__main__':
     pcl_color_frame = basler_fusion_depth_rgb.transform_pcl_to_color_frame(pcl)
     depth_color_frame, _ = basler_fusion_depth_rgb.project_depth_to_color_frame(pcl, color_img)
 
+
     overlay_heatmap, overlay_edges = basler_fusion_depth_rgb.visualize_rgb_depth_alignment(
         color_img, depth_color_frame
     )
-
+    depth_color_frame_heatmap = basler_tof_cam_grab.rawdepth_to_heatmap(depth_color_frame)
+    cv2.imshow("Depth heatmap", depth_color_frame_heatmap)
     cv2.imshow("overlay_heatmap", overlay_heatmap)
-    cv2.imwrite("overlay_heatmap.png", overlay_heatmap)
+    cv2.imwrite("E:/overlay_heatmap.png", overlay_heatmap)
+    cv2.imwrite("E:/depth_heatmap.png", depth_color_frame_heatmap)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
